9485373df0db12cc709173db9ae2b411a9401dcc
unknown <void.iros@gmail.com>
2022-03-21 12:52:06 -0500

    fist comit

---
+++ main.py
@@ -0,0 +1,39 @@
+import os
+import discord
+from discord.ext import commands
+from discord.ui import InputText, Modal, Button
+import requests
+from dotenv import load_dotenv
+
+from pathlib import Path
+
+load_dotenv()
+env_path = Path('.')/'.env'
+load_dotenv(dotenv_path=env_path)
+
+TOKEN = os.getenv("TOKEN")
+
+API_URL = "http://spellbotapi.online/spells"
+
+
+bot = discord.Bot()
+
+
+@bot.slash_command(name="spell", guild_id=(822661824909672449))
+async def show_spell(ctx, message):
+
+    """Search for a dnd Spell by name."""
+    spell_response = requests.get(f"{API_URL}/{message}")
+
+    spell_data = spell_response.json()
+    spell = discord.Embed(
+        title=spell_data["name"], description=spell_data["description"])
+    spell.add_field(name="level", value=spell_data["spell_level"], inline=True)
+    spell.add_field(name="school", value="Conjuration", inline=True)
+    spell.add_field(name="casting time", value=spell_data["casting_time"])
+    spell.add_field(name="range or area", value=spell_data["range_or_area"])
+    spell.add_field(name="Duration", value=spell_data["duration"], inline=True)
+    await ctx.send(embed=spell)
+
+
+bot.run(TOKEN)
